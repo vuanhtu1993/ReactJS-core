3c592ce4e5ddb4eb9cb693d47110dca83c26248a
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-17 13:40:04 +0700

    3. Chuyển tất các function và lưu state trên Game Component 4. Truyền props xuống cho component con để thực thi button và show value

--- src/tictactoe/index.js
+++ src/tictactoe/index.js
@@ -2,13 +2,12 @@ import React, {Component} from 'react';
 
 class Board extends Component {
     renderSquare(i) {
-        return <Square value={i}/>
+        return <Square value={this.props.squares[i]} onClickSquare={() => this.props.onClick(i)}/>
     }
 
     render() {
         return (
-            <div><br/>
-                <div className="status">Status</div>
+            <div>
                 <div className='board-row'>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -31,21 +30,75 @@ class Board extends Component {
 
 function Square(props) {
     return (
-        <button className="square" >{props.value}</button>
+        <button className="square" onClick={() => props.onClickSquare()}>{props.value}</button>
     )
 }
 
 class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            isNext: true,
+        }
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (this.calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = (this.state.isNext === true) ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            isNext: !this.state.isNext,
+        });
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = this.calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = `Winner player ${winner}`;
+        } else {
+            status = `Next player ${current.squares}`;
+        }
         return (
             <div className="game">
                 <h5>Welcome to TicTacToe game</h5>
-                <br/>
                 <div className="game-board">
-                    <Board/>
+                    <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
