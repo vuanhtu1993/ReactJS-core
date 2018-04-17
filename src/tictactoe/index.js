9cecaf97bbba1ca5e33717ee2b4f2d6240c05bf5
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-17 09:52:14 +0700

    1. Xóa constructor tai Board Component 2. Chuyển nơi lưu state lên Game component cha của Board

--- src/tictactoe/index.js
+++ src/tictactoe/index.js
@@ -1,59 +1,14 @@
 import React, {Component} from 'react';
 
 class Board extends Component {
-    constructor() {
-        super();
-        this.state = {
-            squares: Array(9).fill(null),
-            isNext: true,
-        }
-    }
-    handleClick(i) {
-        // copy ra một array square khác
-        const squares = this.state.squares.slice();
-        // gán giá trị
-        squares[i] = (this.state.isNext === true) ? 'X' : 'O';
-        // gán lại vào state
-        this.setState({squares: squares, isNext: !this.state.isNext});
-        console.log(squares);
-    }
-
     renderSquare(i) {
-        return <Square value={this.state.squares[i]}
-        onclickSquare={() => this.handleClick(i)} />
-    }
-
-    calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
+        return <Square value={i}/>
     }
 
     render() {
-        const winner = this.calculateWinner(this.state.squares);
-        let status = '';
-        if (winner) {
-            status = `Winner player is ${winner}`;
-        } else {
-            status = `Next player ${(this.state.isNext === true) ? 'X' : 'O'}`
-        }
         return (
             <div><br/>
-                <div className="status">{status}</div>
+                <div className="status">Status</div>
                 <div className='board-row'>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -76,7 +31,7 @@ class Board extends Component {
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onclickSquare}>{props.value}</button>
+        <button className="square" >{props.value}</button>
     )
 }
 
