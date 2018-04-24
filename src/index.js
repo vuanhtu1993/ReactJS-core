45b663229161e056581bb52fef7b5faf6cec1a45
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-24 13:57:45 +0700

    1. Declare store in index.js 2. Declare provider

--- src/index.js
+++ src/index.js
@@ -5,5 +5,15 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './include/bootstrap'; // or import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// declare redux for application
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import phoneReducer from './assignment1/redux/reducer';
+const store = createStore(phoneReducer);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root'));
 registerServiceWorker();
