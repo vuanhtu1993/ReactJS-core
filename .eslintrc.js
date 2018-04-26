f04155326e1d65c7f1da59888b8084dca24d9707
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-26 14:40:41 +0700

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "airbnb"
+  extends: 'airbnb',
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
+    }
+  },
+  parser: 'babel-eslint',
+  rules: {
+    'no-console': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-string-refs': 'off'
+  }
 };
