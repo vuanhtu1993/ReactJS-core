2c7e226d985df22fd688e67cd28876f577668d2d
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-26 10:57:21 +0700

    the second way to grab value input + set up lint staged

--- src/todoApp/component/todoComponent.js
+++ src/todoApp/component/todoComponent.js
@@ -1,42 +1,45 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Todo extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			input: ''
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+  }
 
-	handleChangeInput = (e) => {
-		this.setState({input: e.target.value})
-	};
-	
-	render() {
-		let todos = this.props.todos;
-		return (
-			<div className="container">
-				Welcome to app
-				<p>Add todos:</p>
-				<input
-					type="text"
-					onChange={this.handleChangeInput}
-					value={this.state.input}/>
-				<button className="btn btn-outline-danger">ADD</button>
-				<p>List todo:</p>
-				<ul>
-					{todos.map((todo) =>
-						<li>
-							{todo.text}
-						</li>)}
-				</ul>
-			</div>
-		)
-	}
+  handleChangeInput = () => {
+    this.setState({ input: this.refs.input.value });
+  };
+
+  handleSubmit = () => {
+    console.log(this.state.input);
+  };
+
+  render() {
+    const { todos } = this.props;
+    return (
+      <div className="container">
+        Welcome to app
+        <p>Add todos:</p>
+        <input
+          type="text"
+          ref="input"
+          onChange={this.handleChangeInput}
+          value={this.state.input}
+        />
+        <button className="btn btn-outline-danger" onClick={this.handleSubmit}>
+          ADD
+        </button>
+        <p>List todo:</p>
+        <ul>{todos.map(todo => <li>{todo.text}</li>)}</ul>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-	todos: state
+const mapStateToProps = state => ({
+  todos: state,
 });
 export default connect(mapStateToProps)(Todo);
