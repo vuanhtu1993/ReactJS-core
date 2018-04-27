54fef3601c629e33c522ff4ef1425d0582273093
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-04-27 11:09:33 +0700

    addition update and delete action

--- src/todoApp/constant.js
+++ src/todoApp/constant.js
@@ -1,5 +1,7 @@
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
 export const VisibilityFilters = {
--- src/todoApp/todoAction.js
+++ src/todoApp/todoAction.js
@@ -1,18 +1,34 @@
-import {ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER} from "./constant";
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
+  DELETE_TODO,
+  UPDATE_TODO,
+} from './constant';
 
-let nextTodoId = 0;
+const nextTodoId = 0;
 export const addTodo = text => ({
-	type: ADD_TODO,
-	id: nextTodoId++,
-	text
+  type: ADD_TODO,
+  id: nextTodoId + 1,
+  text,
 });
 
 export const setVisibilityFilter = filter => ({
-	type: SET_VISIBILITY_FILTER,
-	filter
+  type: SET_VISIBILITY_FILTER,
+  filter,
+});
+
+export const delTodo = id => ({
+  type: DELETE_TODO,
+  id,
+});
+
+export const updateTodo = newTodo => ({
+  type: UPDATE_TODO,
+  newTodo,
 });
 
 export const toggleTodo = id => ({
-	type: TOGGLE_TODO,
-	id
+  type: TOGGLE_TODO,
+  id,
 });
