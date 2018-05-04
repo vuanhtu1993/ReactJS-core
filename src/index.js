acf9f90259d0d86730c329409e613350964a1e9c
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-05-04 10:12:19 +0700

    1. Create store as a props in Provider tag

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './include/bootstrap'; // or import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Provider from 'react-redux';
+import { store } from './Redux/store/store';
+ReactDOM.render((
+	<Provider store={store}>
+		<App />
+	</Provider>
+), document.getElementById('root'));
 registerServiceWorker();
