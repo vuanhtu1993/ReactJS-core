7691c1c5346e52a824eea085e0fbe2d95256c949
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-05-16 11:14:18 +0700

    1. Create child component 2. Declare Switch and Route in main component 3. Declare Link in Header Component

---
+++ src/component/childComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Home = () => (
+	<div>
+		<h2>Welcome home pages</h2>
+	</div>
+);
+
+export const Schedule = () => (
+	<div>
+		<h2>Schedule Component</h2>
+	</div>
+);
+
+export const Roster = () => (
+	<div>
+		<h2>Roster Component</h2>
+	</div>
+);
--- src/component/main.js
+++ src/component/main.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import { Switch, Route } from 'react-router-dom';
+import {Home, Roster, Schedule} from "./childComponent";
+
 class Main extends Component {
 	constructor(props) {
 		super(props)
@@ -7,7 +10,11 @@ class Main extends Component {
 	render() {
 		return (
 			<div>
-				<h3>Welcome to React-router-dom application</h3>
+				<Switch>
+				  <Route exact path="/" component={Home} />
+				  <Route path="/roster" component={Roster} />
+				  <Route path="/schedule" component={Schedule} />
+				</Switch>
 			</div>
 		)
 	}
