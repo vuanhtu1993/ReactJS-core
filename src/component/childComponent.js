8d8eee7f6c6645129d12c7f60de1d1a056a334bd
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-05-16 13:19:22 +0700

    declare <Link> to child component

--- src/component/childComponent.js
+++ src/component/childComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Link, Switch, Route } from 'react-router-dom'
 export const Home = () => (
 	<div>
 		<h2>Welcome home pages</h2>
@@ -13,7 +14,55 @@ export const Schedule = () => (
 );
 
 export const Roster = () => (
+	<Switch>
+		<Route exact path="/roster" component={FullRoster} />
+		<Route exact path="/roster/:number" component={Player} />
+	</Switch>
+);
+
+const PlayerAPI = {
+	players: [
+		{ number: 1, name: "Ben Blocker", position: "G" },
+		{ number: 2, name: "Dave Defender", position: "D" },
+		{ number: 3, name: "Sam Sweeper", position: "D" },
+		{ number: 4, name: "Matt Midfielder", position: "M" },
+		{ number: 5, name: "William Winger", position: "M" },
+		{ number: 6, name: "Fillipe Forward", position: "F" }
+	],
+	all: function() { return this.players},
+	get: function(id) {
+		const isPlayer = p => p.number === id;
+		return this.players.find(isPlayer)
+	}
+};
+
+const FullRoster = () => (
 	<div>
-		<h2>Roster Component</h2>
+		<ul>
+			{
+				PlayerAPI.all().map(p => (
+					<li key={p.number}>
+						<Link to={"/roster/" + p.number} >{p.name}</Link>
+					</li>
+				))
+			}
+		</ul>
 	</div>
 );
+
+const Player = (props) => {
+	const player = PlayerAPI.get(
+		parseInt(props.match.params.number, 10)
+	);
+	console.log(props.history);
+	if (!player) {
+		return <div>Sorry, but the player was not found</div>
+	}
+	return (
+		<div>
+			<h1>{player.name} (#{player.number})</h1>
+			<h2>Position: {player.position}</h2>
+			<Link to='/roster'>Back</Link>
+		</div>
+	)
+};
