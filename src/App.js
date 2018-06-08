7bf0a35aca05ddef8f52e01e62ac8cbeed4fe002
Steve <vuanhtu1993mta@gmail.com>
2018-06-09 00:15:17 +0700

    higher order function apply for show loader when call API(fetch data)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navigation from "./header";
-import Game from "./tictactoe/index";
-import CountDown from "./countdown/countdown";
-import Todos from "./Todos/todos";
+import Article from "./component/Article";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="app">
-                <Navigation/>
-                <h3 className="text-center">Welcome to React application</h3>
-                <div className="container">
-                    <Game/>
-                    <CountDown/>
-                    <Todos/>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="app">
+        <Navigation/>
+        <h3 className="text-center">Welcome to React application</h3>
+        <div className="container">
+          <Article/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
